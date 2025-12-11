53ad217e98e7ecb7e1739dc654a860d8975ae6c8
24051216-cmd <24051216@kiit.ac.in>
2025-12-11 16:59:53 +0530

    init commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,44 +1,31 @@
 import java.util.Scanner;
 
-class Student{
-    String name;
-    int roll_no;
-    String section;
-    String branch;
-    public void info(){
-        Scanner sc= new Scanner(System.in);
-        System.out.println("enter name: ");
-
-        this.name=sc.next();
-
-        System.out.println("enter roll number: ");
-        this.roll_no=sc.nextInt();
-
-        System.out.println("enter section: ");
-        this.section=sc.next();
-
-        System.out.println("enter branch: ");
-        this.branch=sc.next();
-
-
-            }
-    public void Student_info(){
-        System.out.println("Student details are: ");
-        System.out.println(this.name);
-        System.out.println(this.roll_no);
-        System.out.println(this.section);
-        System.out.println(this.branch);
-    }
-
-}
 
 public class Hello {
     public static void main(String[] args){
-        Scanner sc= new Scanner(System.in);
-        Student student1= new Student();
+        Scanner sc= new Scanner (System.in);
+        System.out.print("enter grade of student: ");
+        int grade=sc.nextInt();
 
-        student1.info();
+        if(grade>=90 && grade <= 100){
+            System.out.println("O");
+        }
+        else if(grade<=89 && grade >=80){
+            System.out.println("E");
+        }
+        else if(grade<=79 && grade >=70 ){
+            System.out.println("A");
+        }
+        else if(grade<=69 && grade >= 60){
+            System.out.println("B");
+        }
+        else if (grade <=59 && grade >=50){
+            System.out.println("C");
+        }
+        else if(grade <=49){
+            System.out.println("failed");
+        }
 
-        student1.Student_info();
+
     }
 }
