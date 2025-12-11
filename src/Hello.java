71b67c61ed80133b164d4912025f3afb41de9c39
24051216-cmd <24051216@kiit.ac.in>
2025-12-11 16:49:14 +0530

    init commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
+class Student{
+    String name;
+    int roll_no;
+    String section;
+    String branch;
+    public void info(){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter name: ");
+
+        this.name=sc.next();
+
+        System.out.println("enter roll number: ");
+        this.roll_no=sc.nextInt();
+
+        System.out.println("enter section: ");
+        this.section=sc.next();
+
+        System.out.println("enter branch: ");
+        this.branch=sc.next();
+
+
+            }
+    public void Student_info(){
+        System.out.println("Student details are: ");
+        System.out.println(this.name);
+        System.out.println(this.roll_no);
+        System.out.println(this.section);
+        System.out.println(this.branch);
+    }
+
+}
+
 public class Hello {
     public static void main(String[] args){
-        System.out.println("Hello world");
+        Scanner sc= new Scanner(System.in);
+        Student student1= new Student();
+
+        student1.info();
+
+        student1.Student_info();
     }
 }
