511def7729c0b7e07f2b2bc11662eb2094c9ed65
24051216-cmd <24051216@kiit.ac.in>
2025-12-11 17:22:41 +0530

    init commit

--- src/Hello.java
+++ src/Hello.java
@@ -3,29 +3,17 @@ import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args){
+
         Scanner sc= new Scanner (System.in);
-        System.out.print("enter grade of student: ");
-        int grade=sc.nextInt();
-
-        if(grade>=90 && grade <= 100){
-            System.out.println("O");
+        int arr[] = new int[10];
+        for(int i=0;i<10;i++){
+            arr[i]=sc.nextInt();
         }
-        else if(grade<=89 && grade >=80){
-            System.out.println("E");
+        for(int i=0;i<10;i++){
+            if(arr[i]%2 == 0){
+                System.out.println(arr[i]);
+            }
         }
-        else if(grade<=79 && grade >=70 ){
-            System.out.println("A");
-        }
-        else if(grade<=69 && grade >= 60){
-            System.out.println("B");
-        }
-        else if (grade <=59 && grade >=50){
-            System.out.println("C");
-        }
-        else if(grade <=49){
-            System.out.println("failed");
-        }
-
 
     }
 }
