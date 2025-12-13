ce81ee42b516ba50bb6fe04a10b0805d4b8b1266
24051216-cmd <24051216@kiit.ac.in>
2025-12-13 23:21:56 +0530

    init commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,52 +1,22 @@
 import java.util.Scanner;
 
-/*
+
 
 
 public class Hello {
     public static void main(String[] args){
         Scanner sc= new Scanner (System.in);
         int arr[] = new int[10];
+        System.out.println("enter 10 inputs: ");
         for(int i=0;i<10;i++){
             arr[i]=sc.nextInt();
         }
+        System.out.println("the even elements are: ");
         for(int i=0;i<10;i++){
             if(arr[i]%2 == 0){
                 System.out.println(arr[i]);
             }
         }
 
-    }
-}*/
-
-//2
-
-class Box{
-    int height;
-    int breadth;
-    int length;
-
-    public void volume(){
-        Scanner sc= new Scanner(System.in);
-
-        System.out.println("enter height: ");
-        this.height= sc.nextInt();
-
-        System.out.println("enter length: ");
-        this.length= sc.nextInt();
-
-        System.out.println("enter breadth: ");
-        this.breadth= sc.nextInt();
-
-        int volume= height*length*breadth;
-
-        System.out.println("Volume="+volume);
-    }
-}
-
-public class Hello {
-    public static void main(String[] args){
-        Box box1= new Box();
-        box1.volume();
     }
 }
