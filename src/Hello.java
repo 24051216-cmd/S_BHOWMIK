5cb34a4af1fdd260c591891201e2af8dc9ced425
24051216-cmd <24051216@kiit.ac.in>
2025-12-13 14:18:10 +0530

    init commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
+/*
+
 
 public class Hello {
     public static void main(String[] args){
-
         Scanner sc= new Scanner (System.in);
         int arr[] = new int[10];
         for(int i=0;i<10;i++){
@@ -15,5 +16,37 @@ public class Hello {
             }
         }
 
+    }
+}*/
+
+//2
+
+class Box{
+    int height;
+    int breadth;
+    int length;
+
+    public void volume(){
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("enter height: ");
+        this.height= sc.nextInt();
+
+        System.out.println("enter length: ");
+        this.length= sc.nextInt();
+
+        System.out.println("enter breadth: ");
+        this.breadth= sc.nextInt();
+
+        int volume= height*length*breadth;
+
+        System.out.println("Volume="+volume);
+    }
+}
+
+public class Hello {
+    public static void main(String[] args){
+        Box box1= new Box();
+        box1.volume();
     }
 }
